618524a4b4ac83eb67c7f1edd6a02087de6923d5
Robert de Geus <robert@azavista.com>
2019-09-27 11:37:52 +0200

    changes needed to operate as npm package

--- native/index.js
+++ native/index.js
@@ -106,9 +106,9 @@ var ZebraPrinter = (function (_super) {
     ZebraPrinter = __decorate([
         Plugin({
             pluginName: 'ZebraPrinter',
-            plugin: 'ca-cleversolutions-zebraprinter',
+            plugin: 'azavista-zebraprinter',
             pluginRef: 'cordova.plugins.zebraPrinter',
-            repo: 'git@github.com:cleversolutions/cordova-zebra-printer.git',
+            repo: 'git@github.com:azavista/cordova-zebra-printer.git',
             install: '',
             installVariables: [],
             platforms: ['Android', 'iOS'] // Array of platforms supported, example: ['Android', 'iOS']
